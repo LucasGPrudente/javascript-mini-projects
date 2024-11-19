7401a6f48bf59195e5a79478437d15dfabd503f6
Lucas <lucas.prudente.2004@gmail.com>
2024-11-19 16:26:57 -0300

    Add 'Counter' mini project

---
+++ counter/README.md
@@ -0,0 +1,12 @@
+### Counter
+
+Simple number counter. It is possible to increase <code>+1</code>, decrease <code>-1</code> or reset <code>0</code> the number.
+
+##
+
+### Technologies
+- JavaScript
+- HTML
+- CSS
+
+##
---
+++ counter/assets/js/script.js
@@ -0,0 +1,14 @@
+// increment number in 1
+function increment(){
+  number.innerText = parseInt(number.innerText) + 1;
+}
+
+// decrement number in 1
+function decrement(){
+  number.innerText = parseInt(number.innerText) - 1;
+}
+
+// reset number to 0
+function reset(){
+  number.innerText = 0;
+}
