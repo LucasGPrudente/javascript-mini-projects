273f578fc9729815e4333c9232e946ec3577d0f8
Lucas <lucas.prudente.2004@gmail.com>
2024-11-21 11:34:39 -0300

    Add 'Traffic Light' mini project

---
+++ traffic-light/README.md
@@ -0,0 +1,13 @@
+### Semaphore
+
+Simulation of a semaphore. The lights are alternated whenever the time runs out, following the cycle:
+> `Green` -> `Yellow` -> `Red` -> `Repeat`
+
+##
+
+### Technologies
+- JavaScript
+- HTML
+- CSS
+
+##
---
+++ traffic-light/assets/js/script.js
@@ -0,0 +1,66 @@
+// light
+let greenLight = document.getElementById("green-light");
+let yellowLight = document.getElementById("yellow-light");
+let redLight = document.getElementById("red-light");
+
+// timer
+let timer = document.getElementById("timer");
+
+// turn off all the lights
+greenLight.classList.add("green-off");
+yellowLight.classList.add("yellow-off");
+redLight.classList.add("red-off");
+
+timerGreen();
+
+// turn on the green light and initialize timer from green light
+function timerGreen(){
+  updateTimer(15);
+
+  redLight.classList.remove("red-on");
+  redLight.classList.add("red-off");
+
+  greenLight.classList.remove("green-off");
+  greenLight.classList.add("green-on");
+
+  setTimeout(() => { timerYellow() }, 16 * 1000);
+}
+
+// turn on the yellow light and initialize timer from yellow light
+function timerYellow(){
+  updateTimer(5);
+
+  greenLight.classList.remove("green-on");
+  greenLight.classList.add("green-off");
+
+  yellowLight.classList.remove("yellow-off");
+  yellowLight.classList.add("yellow-on");
+
+  setTimeout(() => { timerRed() }, 6 * 1000);
+}
+
+// turn on the red light and initialize timer from red light
+function timerRed(){
+  updateTimer(15);
+
+  yellowLight.classList.remove("yellow-on");
+  yellowLight.classList.add("yellow-off");
+
+  redLight.classList.remove("red-off");
+  redLight.classList.add("red-on");
+
+  setTimeout(() => { timerGreen() }, 16 * 1000);
+}
+
+// update timer
+function updateTimer(time){
+  timer.innerHTML = (time < 10) ? "0" + time : time;
+  time--;
+
+  let interval = setInterval(() => {
+    timer.innerHTML = (time < 10) ? "0" + time : time;
+    time--;
+
+    if(time < 0){ clearInterval(interval); }
+  }, 1000);
+}
